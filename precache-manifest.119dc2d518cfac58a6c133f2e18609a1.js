e9b15eeda2c59377d5fefec767807571e3f50d0e
anuj-bishtt <anuj.srmcem2310@gmail.com>
2023-04-05 18:45:34 +0530

    Updates

---
+++ precache-manifest.119dc2d518cfac58a6c133f2e18609a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51a92e1f1930afd7cbc7",
+    "url": "/food-order/static/css/main.00c8dc52.chunk.css"
+  },
+  {
+    "revision": "51a92e1f1930afd7cbc7",
+    "url": "/food-order/static/js/main.51a92e1f.chunk.js"
+  },
+  {
+    "revision": "4f471dbef0c68e5a734f",
+    "url": "/food-order/static/js/1.4f471dbe.chunk.js"
+  },
+  {
+    "revision": "4fe385674a5a131ddebe",
+    "url": "/food-order/static/js/runtime~main.4fe38567.js"
+  },
+  {
+    "revision": "55c749900b91566d7fa1eea3937e0e04",
+    "url": "/food-order/static/media/meals.55c74990.jpg"
+  },
+  {
+    "revision": "bbcd15a58b82e0ae33957a1e576ae4ab",
+    "url": "/food-order/index.html"
+  }
+];

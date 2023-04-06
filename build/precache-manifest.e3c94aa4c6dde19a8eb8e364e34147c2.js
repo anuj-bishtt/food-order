466dc72542894f7b93c9b30069c427889ab4e47d
anuj-bishtt <anuj.srmcem2310@gmail.com>
2023-04-06 15:22:21 +0530

    first commit

---
+++ build/precache-manifest.e3c94aa4c6dde19a8eb8e364e34147c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01156b332e0c33af4a46",
+    "url": "/food-order/static/css/main.588ab747.chunk.css"
+  },
+  {
+    "revision": "01156b332e0c33af4a46",
+    "url": "/food-order/static/js/main.01156b33.chunk.js"
+  },
+  {
+    "revision": "4f471dbef0c68e5a734f",
+    "url": "/food-order/static/js/1.4f471dbe.chunk.js"
+  },
+  {
+    "revision": "4fe385674a5a131ddebe",
+    "url": "/food-order/static/js/runtime~main.4fe38567.js"
+  },
+  {
+    "revision": "55c749900b91566d7fa1eea3937e0e04",
+    "url": "/food-order/static/media/meals.55c74990.jpg"
+  },
+  {
+    "revision": "1faf3a94349d9740371d596db3405e1a",
+    "url": "/food-order/index.html"
+  }
+];
